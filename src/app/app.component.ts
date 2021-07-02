3ca00c5300bbfe849cfe1380da2120ea695675f6
Andrey Zhygarev <infernalsteel666@gmail.com>
2021-07-02 17:24:58 +0300

    basic structure done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'angular-ngrx';
+export class AppComponent implements OnInit, OnDestroy {
+  constructor() {}
+  stateCounter: number = 0;
+
+  increment(): void {
+    this.stateCounter++;
+  }
+
+  decrement(): void {
+    this.stateCounter--;
+  }
+
+  ngOnInit() {}
+
+  ngOnDestroy() {}
 }
